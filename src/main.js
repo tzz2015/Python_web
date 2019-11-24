f115fe2e4a9574ad79c4ca799e68ffad19906a5f
啊飞_18133 <1374329135@qq.com>
2019-11-24 10:04:13 +0800

    替换图片

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Element from 'element-ui';
+import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import CommonUtils from './util/CommonUtils.js'
 import ConstMange from './ConstMange.js'
